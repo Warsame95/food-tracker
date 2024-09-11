b45e14d696143cc2875a07e7a6f7be9caa412d2c
Warsame <warsameahmed95@gmail.com>
2024-09-11 23:20:49 +0100

    Completed the add() function in routes to collect inputted data

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from forms import FoodTrackerForm
 from app import app
 
 @app.route('/')
@@ -9,4 +10,15 @@ def index():
 
 @app.route('/add')
 def add():
-    return render_template("add.html")
+    foodtracker_form = FoodTrackerForm
+
+    if foodtracker_form.validate_on_submit():
+        food_name = foodtracker_form.food_name.data
+        quantity = foodtracker_form.quantity.data
+        carbs = foodtracker_form.carbs.data
+        protein = foodtracker_form.protein.data
+        fats = foodtracker_form.fats.data
+        date = foodtracker_form.date.data
+
+
+    return render_template("add.html", template_form = foodtracker_form)
