255bc9c8cb58070d93391ad038b3e4043930a140
Warsame <warsameahmed95@gmail.com>
2024-09-25 01:12:22 +0100

    Added a file with food and log models

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from __init__ import db
+
+class Food(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    calories = db.Column(db.Integer, nullable=False)
+    carbs = db.Column(db.Integer, nullable=False)
+    protein = db.Column(db.Integer, nullable=False)
+    fats = db.Column(db.Integer, nullable=False)
+
+
+class Log(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    foods = db.relationship('Food', secondary=log_food, lazy='dynamic')
