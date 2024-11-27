fa7b9aa4ae50a1641424ade4d7d58d79c3e87efb
Warsame <warsameahmed95@gmail.com>
2024-11-27 23:09:56 +0000

    I added the join table to establish the many to many relationship between log and food again

--- app/__init__.py
+++ app/__init__.py
@@ -8,10 +8,4 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy()
 
-db.init_app(app)
-from app.models import *
-with app.app_context():
-    db.create_all()
-
-
 from app import routes
--- app/models.py
+++ app/models.py
@@ -1,5 +1,10 @@
 from .__init__ import db
 
+log_food = db.Table('log_food',
+    db.Column('log_id', db.Integer, db.ForeignKey('log.id'), primary_key=True),
+    db.Column('food_id', db.Integer, db.ForeignKey('food.id'), primary_key=True)
+)
+
 class Food(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
