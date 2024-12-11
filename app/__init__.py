ce89430fd52bfa7cf0bdce1e2bec7c8715c5dfab
Warsame <warsameahmed95@gmail.com>
2024-12-11 22:07:05 +0000

    Added models to the init.py file and initialised the database

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,26 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDB.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy()
+db = SQLAlchemy(app)
+
+log_food = db.Table('log_food',
+    db.Column('log_id', db.Integer, db.ForeignKey('log.id'), primary_key=True),
+    db.Column('food_id', db.Integer, db.ForeignKey('food.id'), primary_key=True)
+)
+
+class Food(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    calories = db.Column(db.Integer, nullable=False)
+    carbs = db.Column(db.Integer, nullable=False)
+    protein = db.Column(db.Integer, nullable=False)
+    fats = db.Column(db.Integer, nullable=False)
+
+
+class Log(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    foods = db.relationship('Food', secondary= log_food, lazy='dynamic')
 
 from app import routes
